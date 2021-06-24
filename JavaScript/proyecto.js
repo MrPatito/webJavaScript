c83943c7d69b09940edcaf466c65596a9b1f1b76
Franco <franco.hirschfeldt@gmail.com>
2021-06-23 23:56:56 -0300

    Botones interactivos
    Estan en productos

--- JavaScript/proyecto.js
+++ JavaScript/proyecto.js
@@ -17,91 +17,171 @@ const milonga = new modelo ("100cm X 15cm X 7cm", "18 unidades", "1000 Watts", "
 
 const carrito = []
 const carritoPrecio = []
-function agDosItems(){
+let total = carritoPrecio.reduce((a, b) => a + b, 0);
+
+//Let Botones
+
+
+let boton2 = document.getElementById("selectProdu2")
+boton2.onclick = () =>{carrito.push(dosci)
+    carritoPrecio.push(dosci["precio"])
+    let total = carritoPrecio.reduce((a, b) => a + b, 0);
+    console.log(total);
     
-    for (let i = 1; i<5; i++){
-        let producto = prompt("Seleccione el producto colocando un numero segun corresponda:\n 1 = 200w;\n 2 = 300w;\n 3 = 400w;\n 4 = 800w;\n 5 = 1000w");
-        let productoParse = parseInt(producto);
+    document.getElementById("respuesta").innerHTML = `
+    <h2>El total de su carrito es: </h2>
+    <p>$${total}</p>`}
+    
+
+
+let boton3 = document.getElementById("selectProdu3")
+boton3.onclick = () =>{carrito.push(treci)
+    carritoPrecio.push(treci["precio"])
+    let total = carritoPrecio.reduce((a, b) => a + b, 0);
+    console.log(total);
+    
+    document.getElementById("respuesta").innerHTML = `
+    <h2>El total de su carrito es: </h2>
+    <p>$${total}</p>`}
+
+let boton4 = document.getElementById("selectProdu4")
+boton4.onclick = () =>{carrito.push(cuatri)
+    carritoPrecio.push(cuatri["precio"])
+    let total = carritoPrecio.reduce((a, b) => a + b, 0);
+    console.log(total);
+
+    document.getElementById("respuesta").innerHTML = `
+    <h2>El total de su carrito es: </h2>
+    <p>$${total}</p>`}
+
+let boton8 = document.getElementById("selectProdu8")
+boton8.onclick = () =>{carrito.push(ochi)
+    carritoPrecio.push(ochi["precio"])
+    let total = carritoPrecio.reduce((a, b) => a + b, 0);
+    console.log(total);
+
+    document.getElementById("respuesta").innerHTML = `
+    <h2>El total de su carrito es: </h2>
+    <p>$${total}</p>`}
+
+let boton10 = document.getElementById("selectProdu10")
+boton10.onclick = () =>{carrito.push(milonga)
+    carritoPrecio.push(milonga["precio"])
+    let total = carritoPrecio.reduce((a, b) => a + b, 0);
+    console.log(total);
+
+    document.getElementById("respuesta").innerHTML = `
+    <h2>El total de su carrito es: </h2>
+    <p>$${total}</p>`}
+
+
+
+
+
+console.log(total);
+
+    // function agDosItems(){
+    
+//     for (let i = 1; i<5; i++){
+//         let producto = prompt("Seleccione el producto colocando un numero segun corresponda:\n 1 = 200w;\n 2 = 300w;\n 3 = 400w;\n 4 = 800w;\n 5 = 1000w");
+//         let productoParse = parseInt(producto);
         
     
-        function agItem (){
-            if (productoParse === 1){
-                carrito.push(dosci)
+//         function agItem (){
+//             if (productoParse === 1){
+//                 carrito.push(dosci)
               
-            }
-            else if (productoParse === 2){
-                carrito.push(treci)
+//             }
+//             else if (productoParse === 2){
+//                 carrito.push(treci)
                 
-            }
-            else if (productoParse === 3){
-                carrito.push(cuatri)
+//             }
+//             else if (productoParse === 3){
+//                 carrito.push(cuatri)
              
-            }
-            else if (productoParse === 4){
-                carrito.push(ochi)
+//             }
+//             else if (productoParse === 4){
+//                 carrito.push(ochi)
                 
-            }
-            else if (productoParse === 5){
-                carrito.push(milonga)
+//             }
+//             else if (productoParse === 5){
+//                 carrito.push(milonga)
                 
-            }
-            else if (productoParse === 0){
-                return(0)
-            }
+//             }
+//             else if (productoParse === 0){
+//                 return(0)
+//             }
         
-        }
-        agItem()
+//         }
+//         agItem()
         
-        function agPrecio (){
-            if (productoParse === 1){
-                carritoPrecio.push(dosci["precio"])
+//         function agPrecio (){
+//             if (productoParse === 1){
+//                 carritoPrecio.push(dosci["precio"])
               
-            }
-            else if (productoParse === 2){
-                carritoPrecio.push(treci["precio"])
+//             }
+//             else if (productoParse === 2){
+//                 carritoPrecio.push(treci["precio"])
                 
-            }
-            else if (productoParse === 3){
-                carritoPrecio.push(cuatri["precio"])
+//             }
+//             else if (productoParse === 3){
+//                 carritoPrecio.push(cuatri["precio"])
              
-            }
-            else if (productoParse === 4){
-                carritoPrecio.push(ochi["precio"])
+//             }
+//             else if (productoParse === 4){
+//                 carritoPrecio.push(ochi["precio"])
                 
-            }
-            else if (productoParse === 5){
-                carritoPrecio.push(milonga["precio"])
+//             }
+//             else if (productoParse === 5){
+//                 carritoPrecio.push(milonga["precio"])
                 
-            }
-            else if (productoParse === 0){
-                return(0)
-            }
+//             }
+//             else if (productoParse === 0){
+//                 return(0)
+//             }
         
-        }
-        agPrecio()
-    }
-}
+//         }
+//         agPrecio()
+//     }
+// }
 
 
-agDosItems()
+// agDosItems()
 
-for (const modelo of carrito){
-    alert("Perfecto... Seleccionaste los siguientes modelos" + " " + modelo.watts) 
-    console.log(modelo.watts)
+// for (const modelo of carrito){
+//     alert("Perfecto... Seleccionaste los siguientes modelos" + " " + modelo.watts) 
+//     console.log(modelo.watts)
    
-    console.log(modelo["precio"]);
+//     console.log(modelo["precio"]);
+       
+// }
+
+
+// let precioTotal = carritoPrecio[0] + carritoPrecio[1] + carritoPrecio[2] + carritoPrecio[3];
+// console.log(precioTotal)
+// carritoPrecio.sort();
+// console.log(carritoPrecio);
+
+
+
+
+
+
+
+// for(let element of misProductos){
     
-    
-}
-let precioTotal = modelo(precio[0]) + modelo(precio[1]) + modelo(precio[2]) + modelo(precio[3]);
-console.log(precioTotal)
-carritoPrecio.sort();
-console.log(carritoPrecio);
+//     let div = document.createElement('div');
 
-for (const modelo of carrito){
-let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<h6> ${carritoPrecio}</h6>`;
-    document.body.appendChild(contenedor);
-}
+//     div.className = 'card '
 
+//     div.innerHTML = `
+//     <img class="img-fluid center" src=${element.img}>
+//     <h2>${element.titulo}</h2>
+//     <p>${element.precio}</p>
+//     <button class="btn btn-danger">Agregar al carrito</button>`
 
+//     console.log(div);
+
+//     section.appendChild(div);
+
+// }
