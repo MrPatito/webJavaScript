5ccfe49e57093797e73c294af69c354a31805b5a
Franco <franco.hirschfeldt@gmail.com>
2021-06-28 00:37:48 -0300

    AgregueJSON
    Agregue JSON

--- JavaScript/proyecto.js
+++ JavaScript/proyecto.js
@@ -15,9 +15,14 @@ const cuatri = new modelo ("50cm X 15cm X 7cm", "8 unidades", "400 Watts", "2300
 const ochi = new modelo ("75cm X 15cm X 7cm", "16 unidades", "800 Watts", "27000")
 const milonga = new modelo ("100cm X 15cm X 7cm", "18 unidades", "1000 Watts", "33000")
 
+
 const carrito = []
 const carritoPrecio = []
 let total = carritoPrecio.reduce((a, b) => a + b, 0);
+
+
+
+
 
 //Let Botones
 
@@ -27,9 +32,19 @@ boton2.onclick = () =>{carrito.push(dosci)
     carritoPrecio.push(dosci["precio"])
     let total = carritoPrecio.reduce((a, b) => a + b, 0);
     console.log(total);
+
+    localStorage.setItem("carrito", JSON.stringify(carritoPrecio));
+
+    let carritoP = localStorage.getItem('carrito')
+    carritoPParse = JSON.parse(carritoP)
+    console.log(carrito)
+    console.log(carritoPrecio)
+    console.log(carritoP)
+    console.log(carritoPParse)
+   
     
-    document.getElementById("respuesta").innerHTML = `
-    <h2>El total de su carrito es: </h2>
+    document.getElementById("respuesta").innerHTML = 
+    `<h2>El total de su carrito es: </h2>
     <p>$${total}</p>`}
     
 
@@ -39,6 +54,15 @@ boton3.onclick = () =>{carrito.push(treci)
     carritoPrecio.push(treci["precio"])
     let total = carritoPrecio.reduce((a, b) => a + b, 0);
     console.log(total);
+
+    localStorage.setItem("carrito", JSON.stringify(carritoPrecio));
+
+    let carritoP = localStorage.getItem('carrito')
+    carritoPParse = JSON.parse(carritoP)
+    console.log(carrito)
+    console.log(carritoPrecio)
+    console.log(carritoP)
+    console.log(carritoPParse)
     
     document.getElementById("respuesta").innerHTML = `
     <h2>El total de su carrito es: </h2>
@@ -50,6 +74,15 @@ boton4.onclick = () =>{carrito.push(cuatri)
     let total = carritoPrecio.reduce((a, b) => a + b, 0);
     console.log(total);
 
+    localStorage.setItem("carrito", JSON.stringify(carritoPrecio));
+
+    let carritoP = localStorage.getItem('carrito')
+    carritoPParse = JSON.parse(carritoP)
+    console.log(carrito)
+    console.log(carritoPrecio)
+    console.log(carritoP)
+    console.log(carritoPParse)
+
     document.getElementById("respuesta").innerHTML = `
     <h2>El total de su carrito es: </h2>
     <p>$${total}</p>`}
@@ -59,6 +92,15 @@ boton8.onclick = () =>{carrito.push(ochi)
     carritoPrecio.push(ochi["precio"])
     let total = carritoPrecio.reduce((a, b) => a + b, 0);
     console.log(total);
+
+    localStorage.setItem("carrito", JSON.stringify(carritoPrecio));
+
+    let carritoP = localStorage.getItem('carrito')
+    carritoPParse = JSON.parse(carritoP)
+    console.log(carrito)
+    console.log(carritoPrecio)
+    console.log(carritoP)
+    console.log(carritoPParse)
 
     document.getElementById("respuesta").innerHTML = `
     <h2>El total de su carrito es: </h2>
@@ -70,6 +112,15 @@ boton10.onclick = () =>{carrito.push(milonga)
     let total = carritoPrecio.reduce((a, b) => a + b, 0);
     console.log(total);
 
+    localStorage.setItem("carrito", JSON.stringify(carritoPrecio));
+
+    let carritoP = localStorage.getItem('carrito')
+    carritoPParse = JSON.parse(carritoP)
+    console.log(carrito)
+    console.log(carritoPrecio)
+    console.log(carritoP)
+    console.log(carritoPParse)
+
     document.getElementById("respuesta").innerHTML = `
     <h2>El total de su carrito es: </h2>
     <p>$${total}</p>`}
@@ -80,108 +131,5 @@ boton10.onclick = () =>{carrito.push(milonga)
 
 console.log(total);
 
-    // function agDosItems(){
-    
-//     for (let i = 1; i<5; i++){
-//         let producto = prompt("Seleccione el producto colocando un numero segun corresponda:\n 1 = 200w;\n 2 = 300w;\n 3 = 400w;\n 4 = 800w;\n 5 = 1000w");
-//         let productoParse = parseInt(producto);
-        
-    
-//         function agItem (){
-//             if (productoParse === 1){
-//                 carrito.push(dosci)
-              
-//             }
-//             else if (productoParse === 2){
-//                 carrito.push(treci)
-                
-//             }
-//             else if (productoParse === 3){
-//                 carrito.push(cuatri)
-             
-//             }
-//             else if (productoParse === 4){
-//                 carrito.push(ochi)
-                
-//             }
-//             else if (productoParse === 5){
-//                 carrito.push(milonga)
-                
-//             }
-//             else if (productoParse === 0){
-//                 return(0)
-//             }
-        
-//         }
-//         agItem()
-        
-//         function agPrecio (){
-//             if (productoParse === 1){
-//                 carritoPrecio.push(dosci["precio"])
-              
-//             }
-//             else if (productoParse === 2){
-//                 carritoPrecio.push(treci["precio"])
-                
-//             }
-//             else if (productoParse === 3){
-//                 carritoPrecio.push(cuatri["precio"])
-             
-//             }
-//             else if (productoParse === 4){
-//                 carritoPrecio.push(ochi["precio"])
-                
-//             }
-//             else if (productoParse === 5){
-//                 carritoPrecio.push(milonga["precio"])
-                
-//             }
-//             else if (productoParse === 0){
-//                 return(0)
-//             }
-        
-//         }
-//         agPrecio()
-//     }
-// }
 
 
-// agDosItems()
-
-// for (const modelo of carrito){
-//     alert("Perfecto... Seleccionaste los siguientes modelos" + " " + modelo.watts) 
-//     console.log(modelo.watts)
-   
-//     console.log(modelo["precio"]);
-       
-// }
-
-
-// let precioTotal = carritoPrecio[0] + carritoPrecio[1] + carritoPrecio[2] + carritoPrecio[3];
-// console.log(precioTotal)
-// carritoPrecio.sort();
-// console.log(carritoPrecio);
-
-
-
-
-
-
-
-// for(let element of misProductos){
-    
-//     let div = document.createElement('div');
-
-//     div.className = 'card '
-
-//     div.innerHTML = `
-//     <img class="img-fluid center" src=${element.img}>
-//     <h2>${element.titulo}</h2>
-//     <p>${element.precio}</p>
-//     <button class="btn btn-danger">Agregar al carrito</button>`
-
-//     console.log(div);
-
-//     section.appendChild(div);
-
-// }
